213b7fbab88efcd6b81dbdd185f5c3f4c025ce87
Matthew Pomes <the10thwiz@gmail.com>
2021-02-21 14:19:42 -0600

    Write sshd_config and ssh.sh

--- linux/ssh.sh
+++ linux/ssh.sh
@@ -7,10 +7,24 @@
 #
 # Configure installed SSH server
 
-# Edit config file to only allow key based auth
-# Config file is `ssh_config`
+if [[ $EUID -ne 0 ]]
+then
+  printf 'Must be run as root, exiting!\n'
+  exit 1
+fi
 
-# Move old keys to `/ccdc/ssh`
+EXPORT_DIR="/ccdc/ssh"
+
+mkdir -p $EXPORT_DIR
+
+# Edit config file to only allow key based auth
+# Config file is `sshd_config`
+cp /etc/ssh/sshd_config $EXPORT_DIR
+cp /ccdc/scripts/linux/sshd_config /etc/ssh/
+
+# Disable all keys - sshd_config set the server to check this file
+touch /ccdc/ssh/authorized_keys
 
 # Restart service
+systemctl restart ssh
 
