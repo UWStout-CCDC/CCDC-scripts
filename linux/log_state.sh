b07b8b18f075f795b7dd1da7266e060322264ab8
Matthew Pomes <the10thwiz@gmail.com>
2021-02-21 20:11:20 -0600

    Complete log state script

--- linux/log_state.sh
+++ linux/log_state.sh
@@ -12,18 +12,51 @@ EXPORT_DIR="/ccdc"
 
 mkdir -p $EXPORT_DIR
 
-# Export Netstat output
+echo "Netstat"
 netstat -tulnp > $EXPORT_DIR/netstat
 
-# Export user list, including groups
+echo "Users"
+awk -F':' '{
+  printf("%d", $3)
+  printf("\t%s", $1)
+  if ($7 ~ ".*/false" || $7 ~ ".*/nologin") printf("\tnologin")
+  else printf("\t%s", $7)
+  printf("\t%s", $6)
+  username = $1
+  sprintf("grep %s shadow", username) | getline
+  if ($2 ~ "\!\!?|\*") printf("\tlocked")
+  else printf("\t-")
+  sprintf("groups %s", username) | getline
+  groups = $2
+  gsub("[\t ]+", ",", groups)
+  printf("\t%s", substr(groups, 2))
+  print("")
+}' passwd > $EXPORT_DIR/users
 
 # Export Service status
+echo "Services"
+systemctl list-unit-files > $EXPORT_DIR/installed_services
+systemctl list-units > $EXPORT_DIR/running_services
 
 # Export Ip configuration, DNS servers, NTP
+echo "IP config"
+ip addr > $EXPORT_DIR/ip_addr
+ip route > $EXPORT_DIR/ip_route
 
 # Export SSH server settings
+cp /etc/ssh/sshd_config $EXPORT_DIR/sshd_config
 
 # Export Password complexity settings
+if [ -f "/etc/pam.d/system-auth" ]
+then
+  cp /etc/pam.d/system-auth $EXPORT_DIR/system-auth
+fi
+if [ -f "/etc/pam.d/common-password" ]
+then
+  cp /etc/pam.d/common-password $EXPORT_DIR/common-password
+fi
+cp /etc/login.defs $EXPORT_DIR/login.defs
 
 # Export package lists
-apt list > $EXPORT_DIR/packages
+echo "Package lists"
+apt list --installed | grep -v ",automatic" > $EXPORT_DIR/packages
