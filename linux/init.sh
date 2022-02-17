4d2535e5f3c29bb57d8464d3c365579bac9c57e5
chibashr <mrdumez@gmail.com>
2022-02-17 12:14:32 -0600

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -1,3 +1,20 @@
+#!/bin/sh
+#
+# init.sh
+# Copyright (C) 2021 chibashr
+#
+# Distributed under terms of the MIT license.
+# 
+# Script to use during init of linux machine
 
-
+#removes the ability to log on of rogue users
 awk -F: ‘{ if ($3 > 1000) print “usermod -s /bin/nologin “ $1 }’ /etc/passwd > output.sh
+
+#gets wanted username
+echo "What would you like the admin account to be named?"
+read username
+
+#adds user to the wheel group
+sudo usermod -a -G wheel $username
+
+$username passwd
