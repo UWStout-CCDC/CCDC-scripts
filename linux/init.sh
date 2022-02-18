9b1230841ff53a9cd3ffde9b038ef2c0b063776c
Matthew Pomes <the10thwiz@gmail.com>
2022-02-18 15:21:13 -0600

    Complete script
    Create user and assign them groups sudo and wheel, as well as disable login for system users

--- linux/init.sh
+++ linux/init.sh
@@ -7,14 +7,21 @@
 # 
 # Script to use during init of linux machine
 
-#removes the ability to log on of rogue users
-awk -F: ‘{ if ($3 > 1000) print “usermod -s /bin/nologin “ $1 }’ /etc/passwd > output.sh
+if [[ $EUID -ne 0 ]]
+then
+  printf 'Must be run as root, exiting!\n'
+  exit 1
+fi
 
 #gets wanted username
 echo "What would you like the admin account to be named?"
 read username
 
-#adds user to the wheel group
-sudo usermod -a -G wheel $username
+#removes the ability to log on of rogue users
+awk -F: ‘{ print “usermod -s /bin/nologin “ $1 }’ /etc/passwd > output.sh
+echo "usermod -s /bin/bash $username" >> output.sh
+echo "usermod -s /bin/bash root" >> output.sh
 
-$username passwd
+useradd -G wheel,sudo -m -s /bin/bash -U $username
+
+passwd $username
