2a1aa920af6775617050041c6bd93ba39b59d7c4
Matthew Pomes <the10thwiz@gmail.com>
2022-02-18 16:27:40 -0600

    Add prompts

--- linux/iptables.sh
+++ linux/iptables.sh
@@ -10,8 +10,16 @@
 if [[ $EUID -ne 0 ]]
 then
   printf 'Must be run as root, exiting!\n'
-  exit 1
+  #exit 1
 fi
+
+prompt() {
+  read -p "$1 [y/N]" ans
+  case $ans in
+    y|Y) true ;;
+    *) false ;;
+  esac
+}
 
 # Empty all rules
 iptables -t filter -F
@@ -43,31 +51,55 @@ iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT
 # NTP (server time)
 iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
 
+# Splunk
+iptables -t filter -A OUTPUT -p tcp --dport 8000 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --dport 9997 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --dport 8089 -j ACCEPT
+
 ######## OUTBOUND SERVICES ###############
 
-# HTTP/HTTPS (apache)
-iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
+if prompt "HTTP"
+then
+  # HTTP/HTTPS (apache)
+  iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
+  iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
+fi
 
-# DNS (bind)
-iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT
-iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT
+if prompt "DNS"
+then
+  # DNS (bind)
+  iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT
+  iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT
+fi
 
-# SMTP
-iptables -t filter -A OUTPUT -p tcp --dport 25 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT
+if prompt "Mail"
+then
+  # SMTP
+  iptables -t filter -A OUTPUT -p tcp --dport 25 -j ACCEPT
+  iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT
 
-# POP3
-iptables -t filter -A OUTPUT -p tcp --dport 110 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 110 -j ACCEPT
+  # POP3
+  iptables -t filter -A OUTPUT -p tcp --dport 110 -j ACCEPT
+  iptables -t filter -A INPUT -p tcp --dport 110 -j ACCEPT
 
-# IMAP
-iptables -t filter -A OUTPUT -p tcp --dport 143 -j ACCEPT
-iptables -t filter -A INPUT -p tcp --dport 143 -j ACCEPT
+  # IMAP
+  iptables -t filter -A OUTPUT -p tcp --dport 143 -j ACCEPT
+  iptables -t filter -A INPUT -p tcp --dport 143 -j ACCEPT
+fi
 
-# SSH
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+if prompt "Splunk"
+then
+  iptables -t filter -A INPUT -p tcp --dport 8000 -j ACCEPT
+  iptables -t filter -A INPUT -p tcp --dport 9997 -j ACCEPT
+  iptables -t filter -A INPUT -p tcp --dport 8089 -j ACCEPT
+fi
+
+if prompt "SSH"
+then
+  # SSH
+  iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+  iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+fi
 
 # Allow SQL database
 #iptables -t filter -A OUTPUT -p tcp --dport 3306 -j ACCEPT
