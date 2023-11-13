cc98eda3e9c38988e49dc650b35b07bf3c650779
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-11-13 15:57:51 -0600

    Update splunk.ps1
    Made process for setup a bit more streamlined. Still manual process but easier than editing the file.

--- windows/splunk.ps1
+++ windows/splunk.ps1
@@ -1,25 +1,29 @@
-#TODO:
-# - Add script to run on initial setup script
-# - Automate below tasks
-
 # Install and configure Splunk forwarder
 
-#Will automate eventually but for now change manually:
+#NOTE: Process is still manual but easier than editing the file now
 
-# Replace <domain\username> and <pass> with the desired username and password
-# you want the forwarder to run as
-
-# Change the <username> and <password> to desired username and password for creating
-# an admin user for the forwarder
-
-# Change <host:port> to be the hostname or IP of the splunk server
+#TODO:
+# - Add script to run on initial setup script
+# - Finish automations once IP and Ports are known
 
 # Add any MONITOR_PATH="<directory_path>" ` as needed (the '`' specifies a newline)
 # change <directory_path> to the file you want monitored
 
-msiexec.exe /i splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi AGREETOLICENSE=Yes ` 
-LOGON_USERNAME="<domain\username>" LOGON_PASSWORD="<pass>" RECEIVING_INDEXER="<host:port>" DEPLOYMENT_SERVER="<host:port>" `
+$username = Read-Host -Prompt 'Enter username for new user to run Splunk Forwarder as'
+$password = Read-Host -Prompt 'Enter password for Splunk forwarder user'
+$server = Read-Host -Prompt 'Enter Splunk Server IP'
+$recievePort = Read-Host -Prompt 'Enter Splunk Server recieving index port'
+$deploymentPort = Read-Host -Prompt 'Enter Splunk Server deployment port'
+
+$recieve = $server + ":" + $recievePort
+$deployment = $server + ":" + $deploymentPort
+
+#Used for testing inputs
+#Write-Host "$username, $password, $server, $recievePort, $deploymentPort, $recieve, $deployment"
+
+msiexec.exe /i splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi AGREETOLICENSE=Yes `
+LOGON_USERNAME="$username" LOGON_PASSWORD="$password" RECEIVING_INDEXER="$recieve" DEPLOYMENT_SERVER="$deployment" `
 WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 ENABLEADMON=1 `
-SPLUNKUSERNAME=<username> SPLUNKPASSWORD=<password> `
+SPLUNKUSERNAME=$username SPLUNKPASSWORD=$password `
 MONITOR_PATH="<directory_path>" `
 MONITOR_PATH="<directory_path>"
