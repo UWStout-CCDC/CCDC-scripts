2867917483b9d6e7abde2ba925cdd6e751142b2c
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-11-16 15:47:58 -0600

    Update splunk-forward.ps1
    Added capability to install forwarder installer file automatically

--- windows/splunk-forward.ps1
+++ windows/splunk-forward.ps1
@@ -13,6 +13,8 @@
 # Add any MONITOR_PATH="<directory_path>" ` as needed (the '`' specifies a newline)
 # change <directory_path> to the file you want monitored
 
+Invoke-WebRequest -Uri https://download.splunk.com/products/universalforwarder/releases/9.0.4/windows/splunkforwarder-9.0.4-de405f4a7979-x64-release.msi -OutFile splunkforwarder-9.0.4-de405f4a7979-x64-release.msi
+
 $username = Read-Host -Prompt 'Enter username for new user to run Splunk Forwarder as'
 $password = Read-Host -Prompt 'Enter password for Splunk forwarder user'
 $server = Read-Host -Prompt 'Enter Splunk Server IP'
@@ -25,7 +27,7 @@ $deployment = $server + ":" + $deploymentPort
 #Used for testing inputs
 #Write-Host "$username, $password, $server, $recievePort, $deploymentPort, $recieve, $deployment"
 
-msiexec.exe /i splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi AGREETOLICENSE=Yes `
+msiexec.exe /i splunkforwarder-9.0.4-de405f4a7979-x64-release.msi  AGREETOLICENSE=Yes `
 LOGON_USERNAME="$username" LOGON_PASSWORD="$password" RECEIVING_INDEXER="$recieve" DEPLOYMENT_SERVER="$deployment" `
 WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 ENABLEADMON=1 `
 SPLUNKUSERNAME=$username SPLUNKPASSWORD=$password `
