2020583bcfcb95b615fad8ad9e3e858e9f481bea
NotEnoughAuth <oapryga@gmail.com>
2025-02-01 00:11:54 -0600

    Refactor init.sh script to lock all users except root and sysadmin

--- linux/E-Comm/init.sh
+++ linux/E-Comm/init.sh
@@ -58,31 +58,11 @@ prompt() {
     *) [[ "$def" != "[y/N]" ]] ;;
   esac
 }
+
+
+# Lock all users except root and sysadmin
 USER_LOCK_SCRIPT="$SCRIPT_DIR/linux/user_lock.sh"
-cat <<-EOF > $USER_LOCK_SCRIPT
-if [[ \$EUID -ne 0 ]]
-then
-  printf 'Must be run as root, exiting!\n'
-  exit 1
-fi
-
-# Get a list of users from /etc/passwd, and allow the user to select what users to keep with a simple yes/no prompt
-while read -r line; do
-    # Get the username
-    username=$(echo $line | cut -d: -f1)
-    # Check if the user is root
-    if [ "$username" == "root" ] || [ "$username" == "sysadmin" ]; then
-        # Skip the root user and the sysadmin user
-        continue
-    fi
-    # Ask the user if they want to keep the user only if the user can login
-    if [ $(echo $line | cut -d: -f7) != "/sbin/nologin" ]; then
-        usermod -s /sbin/nologin $username
-        passwd -l $username
-    fi
-done < /etc/passwd
-EOF
-
+wget -O $USER_LOCK_SCRIPT $BASEURL/linux/E-Comm/user_lock.sh
 bash $USER_LOCK_SCRIPT
 
 
