4f7e83b3fa6dd6af75528e42f24aede105d725ed
NoAuth <oapryga@gmail.com>
2025-02-02 14:45:22 -0600

    Add MySQL authentication to logins monitoring script and display MySQL users

--- linux/E-Comm/monitor/logins.sh
+++ linux/E-Comm/monitor/logins.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Get credentials from the user for mysql
+read -p "Enter MySQL root username: " MYSQL_USER
+read -sp "Enter MySQL root password: " MYSQL_PASS
+
+# Check if the user entered the correct credentials
+while ! mysql -u $MYSQL_USER -p$MYSQL_PASS -e "exit" > /dev/null 2>&1
+do
+    echo "Incorrect MySQL root username or password. Please try again."
+    read -p "Enter MySQL root username: " MYSQL_USER
+    read -sp "Enter MySQL root password: " MYSQL_PASS
+done
+
 # Check for current and recent logins
 while true
 do
@@ -9,6 +22,11 @@ do
     echo "RECENT LOGIN SESSIONS:"
     echo "----------------------"
     last | head -n5
+    echo
+    echo
+    echo "MySQL USERS:"
+    echo "------------"
+    mysql -e "SELECT User, Host FROM mysql.user;"
     sleep 5
     clear
 done
