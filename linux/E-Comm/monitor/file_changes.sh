4014094fcf0f42e41da635ebee47e8d33eba3173
NoAuth <oapryga@gmail.com>
2025-02-01 21:45:45 -0600

    Improve file change monitoring script to log output and reduce check frequency

--- linux/E-Comm/monitor/file_changes.sh
+++ linux/E-Comm/monitor/file_changes.sh
@@ -2,6 +2,12 @@
 # Check for changes to important files
 while true
 do
-    aide --check
-    sleep 5
+    aide --check > aide_output.txt
+    if [[ -s aide_output.txt ]];then
+        clear
+        echo "CHANGES TO IMPORTANT FILES:"
+        echo "--------------------------"
+        cat aide_output.txt
+    fi
+    sleep 20
 done
