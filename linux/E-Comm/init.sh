8dc707d96a0da30a01b4ac367ce04a6a45f07b4a
NotEnoughAuth <oapryga@gmail.com>
2025-02-02 17:24:28 -0600

    Update BASEURL in init.sh script

--- linux/E-Comm/init.sh
+++ linux/E-Comm/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASEURL=https://raw.githubusercontent.com/UWStout-CCDC/CCDC-scripts/fix/ecomm-init #TODO: Update this URL to the correct branch
+BASEURL=https://raw.githubusercontent.com/UWStout-CCDC/CCDC-scripts/fix/master
 read -p "Enter the default password for the PrestaShop database: " -s DEFAULT_PRESTA_PASS
 
 # Test if the password is correct
