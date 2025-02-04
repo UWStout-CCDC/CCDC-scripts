d425db744f6be07e8e0716a100f2d7b0b528ffa1
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2025-02-04 10:49:16 -0600

    Enhance init-splunk.sh with security improvements, including Auditd setup, disabling uncommon protocols, and core dump restrictions.

--- linux/splunk/init-splunk.sh
+++ linux/splunk/init-splunk.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-###################################
-##    Splunk Specific Configs    ##
-###################################
+# This script is intended as a automated setup for Splunk on CentOS 7 for the CCDC competition.
+# This makes a number of changes to the syste, to do a baseline setup for the system both security and Splunk wise.
+# Many security configs were taken from our other scritps, others from this blog: https://highon.coffee/blog/security-harden-centos-7/#auditd---audit-daemon
+
+################################
+##    Splunk Specific Init    ##
+################################
 
 if [[ $EUID -ne 0 ]]
 then
@@ -13,7 +17,12 @@ fi
 #BASE_URL="https://raw.githubusercontent.com/UWStout-CCDC/CCDC-scripts/master"
 BASE_URL="https://raw.githubusercontent.com/UWStout-CCDC/CCDC-scripts/splunk-automation" # Used for testing in branch
 
-# Changing default admin password
+# Install script dependencies
+wget $BASE_URL/linux/splunk/CentOS-Base.repo -O CentOS-Base.repo
+wget $BASE_URL/linux/init.sh -O init.sh
+wget $BASE_UEL/linux/splunk/audit.rules
+
+# Changing default Splunk Web UI admin password
 cd /opt/splunk/bin
 echo "Enter Splunk Web UI admin password:"
 read -s admin_password
@@ -21,11 +30,9 @@ echo "Enter new Splunk Web UI admin password:"
 read -s password
 ./splunk edit user admin -auth admin:$admin_password -password $password
 
-
 # Fix repos preemtively
 echo -e "\e[33mFixing repos\e[0m"
 cd ~
-wget $BASE_URL/linux/splunk/CentOS-Base.repo -O CentOS-Base.repo
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
 mv ~/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
 yum clean all
@@ -34,15 +41,14 @@ yum makecache
 
 # Install tools (if not already)
 echo -e "\e[33mInstalling tools\e[0m"
-yum install iptables wget git aide net-tools epel-release -y
+yum install iptables wget git aide net-tools audit audit-libs epel-release -y
 
-# Install scripts
-echo -e "\e[33mInstalling init script\e[0m"
-wget $BASE_URL/linux/init.sh -O init.sh
+# Run init script
+echo -e "\e[33mRunning init script\e[0m"
 chmod +x init.sh
 ./init.sh
 
-# cron and at security
+# Cron and AT security
 echo -e "\e[33mSetting cron and at security\e[0m"
 echo "Locking down Cron"
 touch /etc/cron.allow
@@ -62,6 +68,47 @@ systemctl disable --now sshd
 echo -e "\e[33mSetting up AIDE\e[0m"
 aide --init
 mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+
+# Auditd setup
+echo -e "\e[33mSetting up Auditd\e[0m"
+systemctl enable auditd.service
+systemctl start auditd.service
+
+cat audit.rules >> /etc/audit/audit.rules
+
+# Disable uncommon protocols
+echo -e "\e[33mDisabling uncommon protocols\e[0m"
+echo "install dccp /bin/false" >> /etc/modprobe.d/dccp.conf
+echo "install sctp /bin/false" >> /etc/modprobe.d/sctp.conf
+echo "install rds /bin/false" >> /etc/modprobe.d/rds.conf
+echo "install tipc /bin/false" >> /etc/modprobe.d/tipc.conf
+
+# Disable core dumps for users
+echo -e "\e[33mDisabling core dumps for users\e[0m"
+echo "* hard core 0" >> /etc/security/limits.conf
+
+# Disable core dumps for SUID programs
+echo -e "\e[33mDisabling core dumps for SUID programs\e[0m"
+# Set runtime for fs.suid_dumpable
+sysctl -q -n -w fs.suid_dumpable=0
+
+# If fs.suid_dumpable present in /etc/sysctl.conf, change value to "0"
+#     else, add "fs.suid_dumpable = 0" to /etc/sysctl.conf
+if grep --silent ^fs.suid_dumpable /etc/sysctl.conf ; then
+     sed -i 's/^fs.suid_dumpable.*/fs.suid_dumpable = 0/g' /etc/sysctl.conf
+else
+     echo "" >> /etc/sysctl.conf
+     echo "# Set fs.suid_dumpable to 0 per security requirements" >> /etc/sysctl.conf
+     echo "fs.suid_dumpable = 0" >> /etc/sysctl.conf
+
+# Buffer Overflow Protection
+echo -e "\e[33mSetting up buffer overflow protection\e[0m"
+# Enables exec-shield
+sysctl -w kernel.exec-shield=1
+echo "kernel.exec-shield = 1" >> /etc/sysctl.conf
+# Enable ASLR
+sysctl -q -n -w kernel.randomize_va_space=2
+echo "kernel.randomize_va_space = 2" >> /etc/sysctl.conf
 
 # Enable Splunk reciever
 echo -e "\e[33mEnabling Splunk receiver\e[0m"
