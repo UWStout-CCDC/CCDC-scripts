6f7031e0a4456b563fd3332311a896c0803cabda
NoAuth <oapryga@gmail.com>
2025-02-08 21:52:46 -0600

    Add script to update Apache HTTPD to version 2.4.60

--- linux/E-Comm/init.sh
+++ linux/E-Comm/init.sh
@@ -661,4 +661,86 @@ chmod +x /ccdc/scripts/monitor.sh
 
 cp -R /bkp /etc/frr
 
+
+cat <<-EOF > /ccdc/scripts/linux/update_apache.sh
+#######################################
+#
+#          UPDATE HTTPD TO 2.4.60
+#
+#######################################
+# Check if the httpd version is already 2.4.60
+if httpd -v | grep -q "2.4.60"
+then
+    echo "httpd version is already 2.4.60"
+else
+    systemctl stop httpd
+    echo "Updating httpd to 2.4.60..."
+    # Ensure mod_ssl is installed
+    yum install mod_ssl -y
+
+    # Download pre-requisite packages
+    yum install -y gcc make apr-devel apr-util-devel pcre-devel mod_ssl openssl-devel expat expat-devel
+
+    # Download the httpd 2.4.60 source code
+    wget https://archive.apache.org/dist/httpd/httpd-2.4.60.tar.gz
+    tar -xvf httpd-2.4.60.tar.gz
+    cd httpd-2.4.60
+
+    cd srclib
+
+    sudo wget https://downloads.apache.org/apr/apr-1.7.5.tar.gz
+    sudo tar -xvzf apr-1.7.5.tar.gz
+    mv apr-1.7.5 apr
+    sudo wget https://downloads.apache.org/apr/apr-util-1.6.3.tar.gz
+    sudo tar -xvzf apr-util-1.6.3.tar.gz
+    mv apr-util-1.6.3 apr-util
+    rm -f apr-1.7.5.tar.gz apr-util-1.6.3.tar.gz
+
+    cd ..
+
+    # Run configure script with specified options
+    ./configure --with-included-apr --with-included-apr-util --enable-ssl --enable-so --prefix=/etc/httpd --with-mpm=event --enable-rewrite --enable-mods-shared=all
+    make
+    make install
+    mv /usr/sbin/httpd /root/httpd.old
+    mv httpd /usr/sbin/httpd
+    cd ..
+    rm -rf httpd-2.4.60
+    rm httpd-2.4.60.tar.gz
+    cat <<-EOF > /etc/httpd/conf.modules.d/00-mpm.conf
+# Select the MPM module which should be used by uncommenting exactly
+# one of the following LoadModule lines. See the httpd.conf(5) man
+# page for more information on changing the MPM.
+
+# prefork MPM: Implements a non-threaded, pre-forking web server
+# See: http://httpd.apache.org/docs/2.4/mod/prefork.html
+#
+# NOTE: If enabling prefork, the httpd_graceful_shutdown SELinux
+# boolean should be enabled, to allow graceful stop/shutdown.
+#
+#LoadModule mpm_prefork_module modules/mod_mpm_prefork.so
+
+# worker MPM: Multi-Processing Module implementing a hybrid
+# multi-threaded multi-process web server
+# See: http://httpd.apache.org/docs/2.4/mod/worker.html
+#
+#LoadModule mpm_worker_module modules/mod_mpm_worker.so
+
+# event MPM: A variant of the worker MPM with the goal of consuming
+# threads only for connections with active processing
+# See: http://httpd.apache.org/docs/2.4/mod/event.html
+#
+#LoadModule mpm_event_module modules/mod_mpm_event.so
+EO2
+
+    systemctl start httpd
+fi
+
+#######################################
+#
+#          END HTTPD UPDATE
+#
+#######################################
+EOF
+
 echo "Finished running init.sh, please reboot the system to apply changes"
